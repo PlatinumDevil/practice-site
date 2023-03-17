97f4bf578169257d865b301cc310c060d8d32340
PlatinumDevil <j.docherty@email.com>
2023-03-17 14:51:10 +0000

    inital upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/index.js'
+import HomePage from './components/HomePage/index.js';
 import Journal from './components/Journal/index.js';
 import EntryForm from './components/EntryForm/index.js';
 import EditForm from './components/EditForm/index.js';
@@ -12,6 +13,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
+
+          <Route exact path="/" element={<HomePage />} />
           <Route path="/entries" element={<Journal />} />
           <Route path="/add-entry" element={<EntryForm />} />
           <Route path="/edit" element={<EditForm />} />
